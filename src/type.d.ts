d7a9a76d9dae8c6ffe6cf45f25ed455b5030a4cc
FarrosFR <56761912+farrosfr@users.noreply.github.com>
2026-02-14 06:31:35 +0700

    Update deploy.yml, BaseHead.astro, BaseLayout.astro, type.d.ts

--- src/type.d.ts
+++ src/type.d.ts
@@ -2,3 +2,10 @@ declare module 'virtual:config' {
   const Config: import('astro-pure/types').ConfigOutput
   export default Config
 }
+
+declare global {
+  interface Window {
+    dataLayer: Record<string, any>[]
+    gtag: (...args: any[]) => void
+  }
+}
